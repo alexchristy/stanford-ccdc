79f29536c373d3cffcd3a9b744f14f21c0216e05
axkeller <axkeller@stanford.edu>
2020-03-03 11:37:29 -0800

    Update install.ps1

--- tools/currports/install.ps1
+++ tools/currports/install.ps1
@@ -1,4 +1,4 @@
 (New-Object System.Net.WebClient).DownloadFile("https://www.nirsoft.net/utils/cports-x64.zip", "C:\Users\$env:USERNAME\Desktop\cports-x64.zip")
 Expand-Archive -LiteralPath "C:\Users\$env:USERNAME\Desktop\cports-x64.zip" -DestinationPath "C:\Users\$env:USERNAME\Desktop\currports\"
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/applied-cyber/ccdc/master/tools/currports/cports-active.cfg", "C:\Users\$env:USERNAME\Desktop\currports\cports.cfg")
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/applied-cyber/ccdc/master/tools/currports/cports.cfg", "C:\Users\$env:USERNAME\Desktop\currports\cports.cfg")
 & C:\Users\$env:USERNAME\Desktop\currports\cports.exe
