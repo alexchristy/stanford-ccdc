651c5e8a094fb9add830b8e27ab8061cc3e994d1
axkeller <axkeller@stanford.edu>
2020-03-03 17:34:37 -0800

    Update install.ps1

--- tools/procmon/install.ps1
+++ tools/procmon/install.ps1
@@ -1,4 +1,4 @@
 (New-Object System.Net.WebClient).DownloadFile("https://download.sysinternals.com/files/ProcessMonitor.zip", "C:\Users\$env:USERNAME\Desktop\ProcessMonitor.zip")
 Expand-Archive -LiteralPath "C:\Users\$env:USERNAME\Desktop\ProcessMonitor.zip" -DestinationPath "C:\Users\$env:USERNAME\Desktop\ProcessMonitor\"
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/applied-cyber/ccdc/raw/master/tools/procmon/tcp-udp-all.pmc", "C:\Users\$env:USERNAME\Desktop\ProcessMonitor\tcp-udp-all.pmc"
-& "C:\Users\$env:USERNAME\Desktop\ProcessMonitor\Procmon64.exe" /AcceptEula /LoadConfig tcp-udp-all.pmc
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/applied-cyber/ccdc/raw/master/tools/procmon/tcp-udp-all.pmc", "C:\Users\$env:USERNAME\Desktop\ProcessMonitor\tcp-udp-all.pmc")
+& "C:\Users\$env:USERNAME\Desktop\ProcessMonitor\Procmon64.exe" /AcceptEula /LoadConfig "C:\Users\$env:USERNAME\Desktop\ProcessMonitor\tcp-udp-all.pmc"
