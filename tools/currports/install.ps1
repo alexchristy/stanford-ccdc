06a21e3534ea64be847c111dad6d03dfdcf0afb6
axkeller <axkeller@stanford.edu>
2020-03-26 12:17:24 -0700

    Update install.ps1

--- tools/currports/install.ps1
+++ tools/currports/install.ps1
@@ -1,4 +1,5 @@
 (New-Object System.Net.WebClient).DownloadFile("https://www.nirsoft.net/utils/cports-x64.zip", "C:\Users\$env:USERNAME\Desktop\cports-x64.zip")
-Expand-Archive -LiteralPath "C:\Users\$env:USERNAME\Desktop\cports-x64.zip" -DestinationPath "C:\Users\$env:USERNAME\Desktop\currports\"
+(New-Object System.Net.WebClient).DownloadFile("http://stahlworks.com/dev/unzip.exe", "C:\Users\$env:USERNAME\Desktop\unzip.exe")
+& "C:\Users\$env:USERNAME\Desktop\unzip.exe" "C:\Users\$env:USERNAME\Desktop\cports-x64.zip" -d "C:\Users\$env:USERNAME\Desktop\currports"
 (New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/applied-cyber/ccdc/master/tools/currports/cports.cfg", "C:\Users\$env:USERNAME\Desktop\currports\cports.cfg")
 & C:\Users\$env:USERNAME\Desktop\currports\cports.exe
