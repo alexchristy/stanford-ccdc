a745e6683f72717cde4c25b8f4d7463d56bceae0
axkeller <axkeller@stanford.edu>
2020-03-26 11:49:23 -0700

    Update install.ps1

--- tools/sysmon/install.ps1
+++ tools/sysmon/install.ps1
@@ -1,7 +1,7 @@
-(New-Object System.Net.WebClient).DownloadFile("http://download.sysinternals.com/files/Sysmon.zip", "C:\Windows\Temp\apphelper.zip")
-Expand-Archive -LiteralPath C:\Windows\Temp\apphelper.zip -DestinationPath C:\Windows\Temp\apphelper
-Rename-Item C:\Windows\Temp\apphelper\Sysmon64.exe "AppHelper.exe"
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml", "C:\Windows\Temp\apphelper\app-helper.xml")
-& 'C:\Windows\Temp\apphelper\AppHelper.exe' -accepteula -i C:\Windows\Temp\apphelper\app-helper.xml -n -d "apphelp" 
-sc.exe description apphelp "Application Helper"
+(New-Object System.Net.WebClient).DownloadFile("http://download.sysinternals.com/files/Sysmon.zip", "C:\Users\$env:USERNAME\Desktop\Sysmon.zip")
+(New-Object System.Net.WebClient).DownloadFile("http://stahlworks.com/dev/unzip.exe", "C:\Users\$env:USERNAME\Desktop\unzip.exe")
+& "C:\Users\$env:USERNAME\Desktop\unzip.exe" "C:\Users\$env:USERNAME\Desktop\Sysmon.zip" -d "C:\Users\$env:USERNAME\Desktop\Sysmon"
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/SwiftOnSecurity/sysmon-config/master/sysmonconfig-export.xml", "C:\Users\$env:USERNAME\Desktop\Sysmon\sysmonconfig-export.xml")
+& "C:\Users\$env:USERNAME\Desktop\Sysmon\Sysmon64.exe" -accepteula -i "C:\Users\$env:USERNAME\Desktop\Sysmon\sysmonconfig-export.xml" -n 
 wevtutil sl Microsoft-Windows-Sysmon/Operational /ms:209715200
+# & "C:\Users\$env:USERNAME\Desktop\Sysmon\Sysmon64.exe" -u
