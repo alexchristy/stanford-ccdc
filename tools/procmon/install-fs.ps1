8c6e24df45fe96f4cd735b901cbe66f29156d989
axkeller <axkeller@stanford.edu>
2020-03-27 08:09:29 -0700

    Update install-fs.ps1

--- tools/procmon/install-fs.ps1
+++ tools/procmon/install-fs.ps1
@@ -1,2 +1,2 @@
 # Invokes Powershell install from URL - not safe, but fast.
-$DownloadUrl = Invoke-WebRequest https://raw.githubusercontent.com/applied-cyber/ccdc/master/tools/procmon/install.ps1; Invoke-Expression $($DownloadUrl.Content)
+$DownloadUrl = Invoke-WebRequest https://raw.githubusercontent.com/applied-cyber/ccdc/master/tools/procmon/install-procmon.ps1; Invoke-Expression $($DownloadUrl.Content)
